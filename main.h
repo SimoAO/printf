000346bc0badaeeafd20785aacdff1b5e3c50e8a
souhail10EL <souhailelkhoukhi9@gmail.com>
2023-04-25 07:12:23 -0700

    update main.h file

--- main.h
+++ main.h
@@ -12,7 +12,19 @@
 #define NULL_STRING "(null)"
 #define CONVERT_UNSIGNED 2
 #define CONVERT_LOWERCASE 1
-
+/**
+ * struct parameters - parameters of struct
+ * @unsign: flag unsigned value
+ * @minus_flag: minus_flag specified
+ * @plus_flag: plus_flag specified
+ * @zero_flag: zero_flag specified
+ * @space_flag: space_flag specified
+ * @hashtag_flag: hashtag_flag specified
+ * @precision: field specified
+ * @width: field secified
+ * @l_modifier: if it is specified
+ * @h_modifier: if it is specified
+ */
 typedef struct parameters
 {
 	unsigned int unsign	: 1;
@@ -29,10 +41,15 @@ typedef struct parameters
 	unsigned int h_modifier	: 1;
 } params_t;
 
+/**
+ * struct specifier - token of struct
+ * @specifier: token format
+ * @f: associated function
+ */
 typedef struct specifier
 {
 	char *specifier;
-	int (*f) (va_list, params_t *);
+	int (*f)(va_list, params_t *);
 } specifier_t;
 
 int print_S(va_list ap, params_t *params);
@@ -52,10 +69,22 @@ char *convert(long int num, int base, int flags, params_t *params);
 
 int (*get_specifier(char *s))(va_list ap, params_t *params);
 int get_print_func(char *s, va_list ap, params_t *params);
+int *get_width(char *s, params_t *params, va_list ap);
+int get_flag(char *s, params_t *params);
+int get_modifier(char *s, params_t *params);
 
-
+int print_from_to(char *start, char *stop, char *except);
 int print_rot13(va_list ap, params_t *params);
 int print_rev(va_list ap, params_t *params);
+
+int print_number_right_shift(char *str, params_t *params);
+int print_number_left_shift(char *str, params_t *params);
+int print_number(char *str, params_t *params);
+int _strlen(char *s);
+int _isdigit(int c);
+
+void innit_params(params_t *params, va_list ap);
+char *get_precision(char *p, params_t *params, va_list ap);
 
 int _puts(char *str);
 int _putchar(char c);
